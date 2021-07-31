aaa5fa3d95006365f9f197e3389faa9ad82dc320
skycn3322 <34830820+skycn3322@users.noreply.github.com>
2021-08-01 07:57:54 +0800

    Update diy-part1.sh

--- router-config/lede-master/diy-part1.sh
+++ router-config/lede-master/diy-part1.sh
@@ -12,10 +12,29 @@
 
 # Add a feed source
 # sed -i '$a src-git lienol https://github.com/Lienol/openwrt-package' feeds.conf.default
-
+rm -rf ../lean/luci-theme-argon
 # other
 # rm -rf package/lean/{samba4,luci-app-samba4,luci-app-ttyd}
+sed -i '$a src-git kenzok https://github.com/kenzok8/openwrt-packages' feeds.conf.default    # 常用插件源
+sed -i '$a src-git small https://github.com/kenzok8/small' feeds.conf.default                # 常用插件源_依赖安装
+sed -i '$a src-git serverchan https://github.com/tty228/luci-app-serverchan' feeds.conf.defaul  # 信息推送
+git clone https://github.com/jerrykuku/node-request.git package/lean/node-request  #京东签到依赖
+git clone https://github.com/jerrykuku/luci-app-jd-dailybonus.git package/lean/luci-app-jd-dailybonus  #京东签到
+
+# Add luci-theme-argon
+git clone -b 18.06 https://github.com/jerrykuku/luci-theme-argon.git package/lean/luci-theme-argon
+git clone --depth=1 -b 18.06 https://github.com/jerrykuku/luci-theme-argon
+git clone --depth=1 https://github.com/jerrykuku/luci-app-argon-config.git
+
+
+# Add ServerChan
+git clone --depth=1 https://github.com/tty228/luci-app-serverchan
+
+# netdata汉化版
+# pushd package/lean
+# git clone --depth=1 https://github.com/sirpdboy/luci-app-netdata
 
 # 删除默认插件
 rm -rf ./feeds/luci/applications/luci-app-upnp
 rm -rf ./feeds/luci/applications/luci-app-wol
+rm -rf ./package/lean/wol
